b1f9e0adf8d2950c6d4ba1813eb3603a0a257bbf
Eesh Mishra <EESH.24334@stu.upes.ac.in>
2026-02-21 12:39:10 +0530

    Add linked list implementation in DAY21.c
    Implement a linked list to store and print integers.

---
+++ DAY21.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define Node structure
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    if (n <= 0) {
+        return 0;
+    }
+
+    struct Node *head = NULL, *temp = NULL, *newNode = NULL;
+
+    for (int i = 0; i < n; i++) {
+        int value;
+        scanf("%d", &value);
+
+        // Allocate memory for new node
+        newNode = (struct Node*)malloc(sizeof(struct Node));
+        newNode->data = value;
+        newNode->next = NULL;
+
+        if (head == NULL) {
+            head = newNode;  // First node
+        } else {
+            temp->next = newNode;  // Link previous node
+        }
+
+        temp = newNode;  // Move temp forward
+    }
+
+    // Traverse and print
+    temp = head;
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+
+    return 0;
+}
