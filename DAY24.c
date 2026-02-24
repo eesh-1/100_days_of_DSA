29c1928872fb2512c14a09e2b79bd97a951f4cf2
Eesh Mishra <EESH.24334@stu.upes.ac.in>
2026-02-24 19:30:07 +0530

    Implement linked list with insert and delete operations

---
+++ DAY24.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Node structure
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Create new node
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Insert at end
+struct Node* insertEnd(struct Node* head, int data) {
+    struct Node* newNode = createNode(data);
+
+    if (head == NULL)
+        return newNode;
+
+    struct Node* temp = head;
+    while (temp->next != NULL)
+        temp = temp->next;
+
+    temp->next = newNode;
+    return head;
+}
+
+// Delete first occurrence of key
+struct Node* deleteKey(struct Node* head, int key) {
+    struct Node *temp = head, *prev = NULL;
+
+    // Case 1: key is at head
+    if (temp != NULL && temp->data == key) {
+        head = temp->next;
+        free(temp);
+        return head;
+    }
+
+    // Case 2: search for key
+    while (temp != NULL && temp->data != key) {
+        prev = temp;
+        temp = temp->next;
+    }
+
+    // If key not found
+    if (temp == NULL)
+        return head;
+
+    // Unlink node
+    prev->next = temp->next;
+    free(temp);
+
+    return head;
+}
+
+// Print list
+void printList(struct Node* head) {
+    while (head != NULL) {
+        printf("%d ", head->data);
+        head = head->next;
+    }
+}
+
+int main() {
+    int n, value, key;
+    struct Node* head = NULL;
+
+    // Input size
+    scanf("%d", &n);
+
+    // Input elements
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &value);
+        head = insertEnd(head, value);
+    }
+
+    // Input key
+    scanf("%d", &key);
+
+    // Delete first occurrence
+    head = deleteKey(head, key);
+
+    // Print result
+    printList(head);
+
+    return 0;
+}
