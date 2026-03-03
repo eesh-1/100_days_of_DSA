3db9c1706aa00ab1ba33557444a773a16b0883fa
Eesh Mishra <EESH.24334@stu.upes.ac.in>
2026-03-03 09:30:04 +0530

    Implement stack operations in DAY31.c

---
+++ DAY31.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+#define MAX 1000
+
+int stack[MAX];
+int top = -1;
+
+// Push operation
+void push(int value) {
+    if (top == MAX - 1) {
+        // Optional: handle overflow if required
+        return;
+    }
+    stack[++top] = value;
+}
+
+// Pop operation
+void pop() {
+    if (top == -1) {
+        printf("Stack Underflow\n");
+        return;
+    }
+    printf("%d\n", stack[top--]);
+}
+
+// Display operation (top to bottom)
+void display() {
+    if (top == -1) {
+        return;
+    }
+    for (int i = top; i >= 0; i--) {
+        printf("%d ", stack[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    while (n--) {
+        int type;
+        scanf("%d", &type);
+
+        if (type == 1) {
+            int value;
+            scanf("%d", &value);
+            push(value);
+        }
+        else if (type == 2) {
+            pop();
+        }
+        else if (type == 3) {
+            display();
+        }
+    }
+
+    return 0;
+}
